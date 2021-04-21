f3ce754fef09bcd9a63fba7c06435e475c82944e
wangqiaoxin <793021821@qq.com>
2021-04-21 14:11:24 +0800

    add shape

---
+++ src/components/shape.vue
@@ -0,0 +1,27 @@
+<template>
+  <el-button class="qx-button">{{ data.propValue }}</el-button>
+</template>
+
+<script lang="ts">
+import { IButton } from '@/store'
+import Vue, { PropType } from 'vue'
+
+export default Vue.extend({
+  name: 'qx-button',
+  props: {
+    data: {
+      type: Object as PropType<IButton>,
+      required: true,
+    },
+  },
+
+  data: function () {
+    return {} as IButton
+  },
+})
+</script>
+<style scoped lang="less">
+/deep/ .el-button {
+  padding: 0px;
+}
+</style>
